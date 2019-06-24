0184aeafa70cb6a2039e3c1be65482df5c5de767
shaivpidadi <shaivpidadi@gmail.com>
2019-06-24 12:30:45 +0530

    Added test cases

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,24 +1,96 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+
+// Error: Timeout of 2000ms exceeded. For async tests and hooks, ensure "done()" is called; if returning a Promise, ensure it resolves.
+// To remove above error
+const OPTIONS = {
+    defaultBlock: "latest",
+    transactionConfirmationBlocks: 1,
+    transactionBlockTimeout: 5
+};
+
+
+const web3 = new Web3(ganache.provider(),null, OPTIONS);
 
 const {interface, bytecode} = require('../compile');
 
 let lottery;
 let accounts;
 
-beforeEach(async() => {
+beforeEach(async() => {                     
     accounts = await web3.eth.getAccounts();
-    console.log(await new web3.eth.Contract(JSON.parse(interface)));
     lottery = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode})
         .send({from: accounts[0], gas: '4000000'})
     });
 
 describe('Lottery Contract', () => {
-
     it('deploys a contract',() => {
         assert.ok(lottery.options.address);
     });
+
+    it('allows one account to enter', async() => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(1, players.length);
+    }); 
+
+    it('allows Multiple account to enter', async() => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);  
+        assert.equal(3, players.length);
+    });
+
+    it('requires a minimum amount of ether to enter', async() => {
+        try{
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 0
+            });
+            assert(false);
+        }
+        catch(err) {
+            assert(err)
+        }
+    })
+
+    it('only manager can call pickWinner', async() =>{
+        try{
+            await lottery.methods.pickWinner.send({
+                from: accounts[1]
+            });
+            assert(false); 
+        }
+        catch(err){
+            assert(err);
+        }
+    })
 });
